0f9c95511b2cf15ba8b9b36975daa2586f6c351f
slammyboy <olaa14427@gmail.com>
2023-09-20 13:19:43 -0700

    1-strncat.c1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 	 int k, r;
 	k = 0;
 	r = 0;
-
-        while (dest[k] != '\0')
+	
+	while (dest[k] != '\0')
 	{
 
 		k++;
-	 }
+	}
 
 	while (r < n && src[r] != '\0')
 	{
